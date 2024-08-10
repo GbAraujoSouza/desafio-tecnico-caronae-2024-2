9c969db9961a6181065e744fd4d328a2901d6374
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-10 16:15:14 -0300

    feat: componente RideCard estilizado

--- src/components/RideCard/index.tsx
+++ src/components/RideCard/index.tsx
@@ -1,5 +1,22 @@
-import React from "react";
-import { CardContainer } from "./styles";
+import React, { useState } from "react";
+import {
+  CardContainer,
+  DriverInfo,
+  DriverName,
+  DriverPhoto,
+  Location,
+  SeatsInfo,
+  TravelInfo,
+  TravelLocationInfo,
+  TravelTimeInfo,
+} from "./styles";
+import UsersGreen from "../../assets/users-green.svg";
+import UsersOrange from "../../assets/users-orange.svg";
+import UsersRed from "../../assets/users-red.svg";
+import TargetIcon from "../../assets/target-icon.svg";
+import FlagIcon from "../../assets/flag-icon.svg";
+import LongRoad from "../../assets/long-road.svg";
+import DefaulDriverPhoto from "../../assets/default-user-photo.svg";
 
 interface RideCardProps {
   driverName: string;
@@ -11,6 +28,12 @@ interface RideCardProps {
   seatsAvailable: number;
 }
 
+const getSeatsIcon = (seatsAvailable: number) => {
+  if (seatsAvailable > 2) return UsersGreen;
+  if (seatsAvailable > 0) return UsersOrange;
+  return UsersRed;
+};
+
 const RideCard: React.FC<RideCardProps> = ({
   driverName,
   driverProfileUrl,
@@ -20,15 +43,46 @@ const RideCard: React.FC<RideCardProps> = ({
   arrivalLocation,
   seatsAvailable,
 }) => {
+  const [driverPhoto, setDriverPhoto] = useState<string>(driverProfileUrl);
+
+  const handleNotFoundDriverPhoto = () => {
+    setDriverPhoto(DefaulDriverPhoto)
+  }
+
   return (
     <CardContainer>
-      <img src={driverProfileUrl} width={50} height={50} />
-      <p>{driverName}</p>
-      <p>{departureTime}</p>
-      <p>{departureLocation}</p>
-      <p>{arrivalTime}</p>
-      <p>{arrivalLocation}</p>
-      <p>{seatsAvailable}</p>
+      <DriverInfo>
+        <DriverPhoto
+          src={driverPhoto}
+          alt={`${driverName}'s photo'`}
+          onError={handleNotFoundDriverPhoto}
+        />
+        <DriverName>{driverName}</DriverName>
+        <SeatsInfo $seats={seatsAvailable}>
+          <img src={getSeatsIcon(seatsAvailable)} alt="People icon" />
+          {seatsAvailable > 0 ? seatsAvailable : "Lotado"}
+        </SeatsInfo>
+      </DriverInfo>
+
+      <TravelInfo>
+        <TravelLocationInfo>
+          <Location>
+            <span>{departureLocation}</span>
+            <img src={TargetIcon} alt="Target icon" />
+          </Location>
+
+          <img src={LongRoad} alt="Road image" />
+
+          <Location>
+            <img src={FlagIcon} alt="Flag icon" />
+            <span>{arrivalLocation}</span>
+          </Location>
+        </TravelLocationInfo>
+        <TravelTimeInfo>
+          <span>{departureTime}</span>
+          <span>{arrivalTime}</span>
+        </TravelTimeInfo>
+      </TravelInfo>
     </CardContainer>
   );
 };
--- src/components/RideCard/styles.tsx
+++ src/components/RideCard/styles.tsx
@@ -3,7 +3,75 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 1.5rem;
+  gap: 1.5rem;
+  padding: 1.5rem 1.5rem 1rem;
   border: 1px solid ${({ theme }) => theme.COLORS.NEUTRAL.LIGHT};
   border-radius: 8px;
+  color: ${({ theme }) => theme.COLORS.NEUTRAL.DARKEST};
+  box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.05);
 `;
+
+export const DriverInfo = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  align-self: stretch;
+`;
+
+export const DriverPhoto = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+  background-color: ${({ theme }) => theme.COLORS.NEUTRAL.LIGHT};
+`;
+
+export const DriverName = styled.p`
+  font-weight: 600;
+  flex: 1 0 0;
+`;
+
+interface SeatsInfoProps {
+  $seats: number;
+}
+
+export const SeatsInfo = styled.div<SeatsInfoProps>`
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${({$seats, theme}) => {
+    if ($seats > 2) return theme.COLORS.ACTION.GREEN;
+    if ($seats > 0) return theme.COLORS.ACTION.ORANGE;
+    return theme.COLORS.ACTION.RED;
+  }};
+`;
+
+export const TravelInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const TravelLocationInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Location = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+`;
+
+export const TravelTimeInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-self: stretch;
+  font-size: 1.5rem;
+  font-weight: 600;
+`
