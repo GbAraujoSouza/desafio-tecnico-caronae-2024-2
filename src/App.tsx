e06a3ad521780969f181dbe368bb8bdaad30da98
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-09 23:48:31 -0300

    config: tema e estilos globais configurados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "styled-components"
+import Header from "./components/Header"
+import { GlobalStyle } from "./theme/globalStyles"
+import theme from "./theme"
 function App() {
 
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle /> 
+      <Header />
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+  COLORS: {
+    WHITE: "#fefefe",
+    GRAY: "#E4E7EC",
+    LIGHTBLUE: "#F0F4FE",
+    PRIMARY: "#2B388A",
+    ACTION: {
+      GREEN: "#17B270",
+      ORANGE: "#ED8024",
+      RED: "#EB3D38",
+    },
+    NEUTRAL: {
+      DARKEST: "#171717",
+      DARK: "#464646",
+      LIGHT: "#DFDFDF",
+      LIGHTEST: "#FEFEFE",
+    },
+  },
+};
