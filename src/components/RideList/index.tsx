ebe7df4817b717d4543c3f31be1ba415548d732a
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-11 10:33:25 -0300

    feat: buscar carona com filtro

--- src/components/RideList/index.tsx
+++ src/components/RideList/index.tsx
@@ -11,8 +11,8 @@ import {
   SearchFilter,
 } from "./styles";
 import RideService from "../../services/RideService";
-import CalendarIcon from "../../assets/calendar.svg"
-import SearchIcon from "../../assets/search-icon.svg"
+import CalendarIcon from "../../assets/calendar.svg";
+import SearchIcon from "../../assets/search-icon.svg";
 import { getCurrentDateFormated } from "../../utils/currentDate";
 
 interface RidesData {
@@ -44,6 +44,12 @@ const RideList: React.FC = () => {
 
   const [onGoingFilter, setOnGoingFilter] = useState<boolean>(true);
 
+  const [searchString, setSearchString] = useState<string>("");
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchString(e.target.value);
+  };
+
   const fetchRides = async () => {
     try {
       const response = await RideService.getAllRides();
@@ -71,30 +77,56 @@ const RideList: React.FC = () => {
             {getCurrentDateFormated()}
           </FilterButton>
           <Divider />
-          <FilterButton $selected={onGoingFilter} onClick={() => setOnGoingFilter(true)}>Chegando na UFRJ</FilterButton>
-          <FilterButton $selected={!onGoingFilter} onClick={() => setOnGoingFilter(false)}>Saindo da UFRJ</FilterButton>
+          <FilterButton
+            $selected={onGoingFilter}
+            onClick={() => setOnGoingFilter(true)}
+          >
+            Chegando na UFRJ
+          </FilterButton>
+          <FilterButton
+            $selected={!onGoingFilter}
+            onClick={() => setOnGoingFilter(false)}
+          >
+            Saindo da UFRJ
+          </FilterButton>
         </FilterButtonsSection>
         <SearchFilter>
           <img src={SearchIcon} />
-          <SearchInput type="text" placeholder="Buscar" />
+          <SearchInput
+            type="text"
+            placeholder="Buscar"
+            onChange={handleSearch}
+          />
         </SearchFilter>
       </FilterBar>
       <RideGrid>
-        {rides?.caronas.map((carona, index) => {
-          return (
-            <RideCard
-              key={index}
-              rideId={index}
-              driverName={carona.motorista.nome}
-              driverProfileUrl={carona.motorista.foto}
-              departureTime={carona.horario_partida}
-              arrivalTime={carona.horario_chegada}
-              departureLocation={carona.local_partida}
-              arrivalLocation={carona.local_chegada}
-              seatsAvailable={carona.vagas}
-            />
-          );
-        })}
+        {rides?.caronas
+          .filter((ride) => {
+            return onGoingFilter
+              ? ride.local_chegada === "Fundão"
+              : ride.local_chegada !== "Fundão";
+          })
+          .filter((ride) => {
+            return (
+              ride.local_chegada.includes(searchString) ||
+              ride.local_partida.includes(searchString)
+            );
+          })
+          .map((carona, index) => {
+            return (
+              <RideCard
+                key={index}
+                rideId={index}
+                driverName={carona.motorista.nome}
+                driverProfileUrl={carona.motorista.foto}
+                departureTime={carona.horario_partida}
+                arrivalTime={carona.horario_chegada}
+                departureLocation={carona.local_partida}
+                arrivalLocation={carona.local_chegada}
+                seatsAvailable={carona.vagas}
+              />
+            );
+          })}
       </RideGrid>
     </ListContainer>
   );
