6e9a8c992f3bbc30744936724a95530042e81abb
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-11 17:58:16 -0300

    test: teste unitario feito para SearchInput

---
+++ src/components/RideList/__test__/SearchInput.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { MemoryRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "../../../theme";
+import RideList from "..";
+
+describe("SearchInput testing", () => {
+  it("should render input element", async () => {
+    render(
+      <MemoryRouter>
+        <ThemeProvider theme={theme}>
+          <RideList />
+        </ThemeProvider>
+      </MemoryRouter>,
+    );
+
+    const inputElement = await screen.findByPlaceholderText("Buscar");
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should be able to type in input", async () => {
+    render(
+      <MemoryRouter>
+        <ThemeProvider theme={theme}>
+          <RideList />
+        </ThemeProvider>
+      </MemoryRouter>,
+    );
+
+    const inputElement = await screen.findByPlaceholderText("Buscar");
+    fireEvent.change(inputElement, { target: { value: "Leblon" } });
+    expect(inputElement.value).toBe("Leblon");
+  });
+});
