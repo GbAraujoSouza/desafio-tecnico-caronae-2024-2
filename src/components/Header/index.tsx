e34e4d77d4be85d96b6318b0c78689b6d305a218
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-11 09:58:25 -0300

    feat: adicionada estilização condicional na Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { HeaderContainer, NavBarItems } from "./styles";
+import { HeaderContainer, NavBarItems, StyledNavLink } from "./styles";
 import { Button } from "../Button";
 import CaronaeLogo from '../../assets/Caronae-logo.svg'
+import { useLocation } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const location = useLocation();
+
+  const isRidesPage = location.pathname === "/";
   return (
     <HeaderContainer>
       <img src={CaronaeLogo}/>
       <NavBarItems>
-        <li>Caronas</li>
+        <StyledNavLink to="/" $isActive={isRidesPage}>{isRidesPage ? "Caronas": "Tela Inicial"}</StyledNavLink>
         <li>Historico de Caronas</li>
         <li>Configurações</li>
       </NavBarItems>
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,13 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
   background-color: ${({ theme }) => theme.COLORS.NEUTRAL.LIGHTEST};
   color: ${({ theme }) => theme.COLORS.NEUTRAL.DARK};
-  padding: 19px 112px;
+  padding: 1.188rem 7rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
   gap: 10px;
+  box-shadow: 0px 2px 4px 0px rgba(51, 51, 51, 0.20);
 `;
 
 export const NavBarItems = styled.ul`
@@ -16,5 +18,23 @@ export const NavBarItems = styled.ul`
   list-style: none;
 	margin-left: 0;
 	padding-left: 0;
-`
+`;
+
+interface StyledNavLinkProps {
+  $isActive: boolean;
+}
+
+export const StyledNavLink = styled(NavLink)<StyledNavLinkProps>`
+  color: ${({ $isActive, theme }) => ($isActive ? theme.COLORS.PRIMARY : "inherit")};
+  font-weight: ${({ $isActive }) => ($isActive ? "bold" : "normal")};
+  font-weight: ${({ $isActive }) => ($isActive ? "bold" : "normal")};
+  text-decoration: none;
+  &.active {
+    color: ${({ theme }) => theme.COLORS.PRIMARY};
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.COLORS.PRIMARY};
+  }
+`;
 
